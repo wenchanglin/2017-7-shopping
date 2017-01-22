7caaf0bac710804d2c21c946f985cb72fa99c50e
桐宇非 <18667123395@163.com>
2017-01-22 15:14:38 +0800

    Initial Commit

---
+++ LogisticsDriver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LogisticsDriver
+//
+//  Created by Blues on 17/1/22.
+//  Copyright © 2017年 LongKe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LogisticsDriver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LogisticsDriver
+//
+//  Created by Blues on 17/1/22.
+//  Copyright © 2017年 LongKe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
